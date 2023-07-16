3c549640df64d08f0c793bf7dce957a936c7467f
Kirito-Aus <54315770+Kirito-Aus@users.noreply.github.com>
2023-07-17 00:58:28 +0800

    正则化的步骤

---
+++ Q1/DataPreparing/Normalization.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+# 读取数据
+data = pd.read_csv('../UserUseSituationWithClassZtmp.csv')
+
+# 选择需要处理的列
+cols_to_process = ['duration', 'up_flow', 'down_flow']
+
+# 对所选列进行 Min-Max 规范化
+# 将数据先归一化到[0,1]之间
+data[cols_to_process] = (data[cols_to_process] - data[cols_to_process].min()) / (data[cols_to_process].max() - data[cols_to_process].min())
+
+# 使用Turkey方法，基于箱线图的异常值检测
+# 计算 Q1、Q3 和 IQR，定义阈值 k
+Q1 = data[cols_to_process].quantile(0.25)
+Q3 = data[cols_to_process].quantile(0.75)
+IQR = Q3 - Q1
+k = 1.5
+
+# 使用 Turkey 方法检查异常值，并将数据限制在 [0,1] 范围内
+data[cols_to_process] = np.clip(data[cols_to_process], 0, 1) # 将数据限制在 [0,1] 范围内
+data = data[~((data[cols_to_process] < (Q1 - k * IQR)) | (data[cols_to_process] > (Q3 + k * IQR))).any(axis=1)] # 去除异常值
+
+# 对整个数据集进行 Min-Max 规范化
+data[cols_to_process] = (data[cols_to_process] - data[cols_to_process].min()) / (data[cols_to_process].max() - data[cols_to_process].min())
+
+# 将数据限制在 [0,1] 范围内
+data[cols_to_process] = data[cols_to_process].clip(0, 1)
+
+data.to_csv('normalizedAndCleaned.csv', index=False)
