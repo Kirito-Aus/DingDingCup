86320c14d7d459ec82712988ebe4a558c41a4003
michelle-jy <60495773+michelle-jy@users.noreply.github.com>
2023-07-18 07:17:16 +0800

    Add files via upload

---
+++ Q2/models_Classification.py
@@ -0,0 +1,113 @@
+
+# ---------------决策树---------------
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 加载数据集
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# 划分数据集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 创建决策树分类器
+clf = DecisionTreeClassifier()
+
+# 训练模型
+clf.fit(X_train, y_train)
+
+# 预测结果
+y_pred = clf.predict(X_test)
+
+# 计算准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+
+# ---------------支持向量机---------------
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 加载数据集
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# 划分数据集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 创建SVM分类器
+clf = SVC(kernel='linear')
+
+# 训练模型
+clf.fit(X_train, y_train)
+
+# 预测结果
+y_pred = clf.predict(X_test)
+
+# 计算准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+
+
+# ---------------逻辑回归---------------
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 加载数据集
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# 划分数据集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 创建逻辑回归分类器
+clf = LogisticRegression()
+
+# 训练模型
+clf.fit(X_train, y_train)
+
+# 预测结果
+y_pred = clf.predict(X_test)
+
+# 计算准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+
+
+# ---------------K近邻---------------
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# 加载数据集
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# 划分数据集
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 创建K近邻分类器
+clf = KNeighborsClassifier(n_neighbors=3)
+
+# 训练模型
+clf.fit(X_train, y_train)
+
+# 预测结果
+y_pred = clf.predict(X_test)
+
+# 计算准确率
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
